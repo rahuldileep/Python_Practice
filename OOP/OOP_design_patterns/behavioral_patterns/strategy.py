22937b8432cde07baa2c11fb0a2ec30291a2480d
Rahul <rahuldileep@gmail.com>
2023-06-19 18:34:10 -0700

    updated comments

--- OOP/OOP_design_patterns/behavioral_patterns/strategy.py
+++ OOP/OOP_design_patterns/behavioral_patterns/strategy.py
@@ -1,4 +1,4 @@
-# Strategy is a behavioral design pattern that turns a set of behaviors into objects and makes them interchangeable inside original context object.
+# Strategy is a behavioral design pattern that turns a set of behaviors into objects and 
 
 # The original object, called context, holds a reference to a strategy object. The context delegates executing the behavior to the linked strategy object. 
 # In order to change the way the context performs its work, other objects may replace the currently linked strategy object with another one.
